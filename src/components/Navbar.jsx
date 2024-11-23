5375c50d7842aa30efbfcfbdf12ccad44d9704d2
Deepakkumar <110596459+Deepakkumar08@users.noreply.github.com>
2024-11-23 17:43:54 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
   return (
     <nav className='mb-5 flex item-center justify-between py-3'>
       <div className='flex flex-shrink-0 items-center'>
-         <h2 className='font-bold text-white mx-3 text-2xl'>Dv</h2>
+         <h2 className='font-bold text-white mx-3 text-2xl'>RK</h2>
 
       </div>
       <div className='flex gap-6 justify-center items-center m-4 text-2xl'>
-          <a href='https://www.linkedin.com/in/deepakkumar-vaitheeswaran-2672451aa/' target='_blank'><FaLinkedin/></a>  
-          <a href='https://github.com/Deepakkumar08'target='_blank' >        <FaGithub/></a>    
+          <a href='https://www.linkedin.com/in/roshini-kumar-65441817b/' target='_blank'><FaLinkedin/></a>  
+          <a href='https://github.com/Roshinikumar'target='_blank' >        <FaGithub/></a>    
         {/* <FaInstagram></FaInstagram> */}
       </div>
     </nav>
